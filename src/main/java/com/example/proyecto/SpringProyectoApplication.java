dfa4721f29cbabad7e0c61815969d21812e1fc35
DiegoDLPaz <delapazgonzalezdiego@gmail.com>
2023-02-25 12:54:36 +0000

    Ahora funciona

--- src/main/java/com/example/proyecto/SpringProyectoApplication.java
+++ src/main/java/com/example/proyecto/SpringProyectoApplication.java
@@ -2,9 +2,17 @@ package com.example.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+@EntityScan("com.example.modelo")
+@ComponentScan("com.example.controlador")
+@EnableJpaRepositories("com.example.repositorio")
+
 public class SpringProyectoApplication {
 
 	public static void main(String[] args) {
