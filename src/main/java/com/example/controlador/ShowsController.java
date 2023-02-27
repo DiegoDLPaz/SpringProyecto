cff72a84bd77de9d4705a200998f6b5648313883
DiegoDLPaz <delapazgonzalezdiego@gmail.com>
2023-02-27 11:13:06 +0000

    Meto el metodo para actualizar

--- src/main/java/com/example/controlador/ShowsController.java
+++ src/main/java/com/example/controlador/ShowsController.java
@@ -1,6 +1,7 @@
 package com.example.controlador;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +26,22 @@ public class ShowsController {
 	
 	// GET + POST + DELETE + PUT
 	@GetMapping("shows")
-	List<Shows> getAllshows(@RequestParam(name = "tipo",required = false)
-	String tipoParametro){
-		if(tipoParametro == null) return repo.findAll();
+	List<Shows> getAllshows(@RequestParam(name = "titulo",required = false)
+	String tituloParametro){
+		if(tituloParametro == null) return repo.findAll();
 		else {
-			return repo.findBytipoStartingWith(tipoParametro);
+			return repo.findBytipoStartingWith(tituloParametro);
 		}
 	}
 	
-	@GetMapping("shows/{showId}")
-	Shows getShowById(@PathVariable("showId") String idRecibido) {
-		return repo.findById(idRecibido).get();
+	@GetMapping("shows/{titulo}")
+	Shows getShowById(@PathVariable("titulo") String tituloRecibido) {
+		return repo.findByTitulo(tituloRecibido);
 	}
 	
-	@DeleteMapping("shows/{showid}")
-	ResponseEntity<String> borrarShows(@PathVariable("showId") String idRecibido){
-		this.repo.deleteById(idRecibido);
+	@DeleteMapping("shows/{titulo}")
+	ResponseEntity<?> borrarShowsTitulo(@PathVariable("titulo") String tituloRecibido){		
+		this.repo.deleteByTitulo(tituloRecibido);
 		return new ResponseEntity<String>("El show se ha borrado correctamente",HttpStatus.OK);
 	}
 	
@@ -48,5 +50,28 @@ public class ShowsController {
 			this.repo.save(nuevosShow);
 			return new ResponseEntity<String>("El show se ha añadido correctamente",HttpStatus.OK);
 	}
+	
+	@PutMapping("shows/{titulo}")
+    public ResponseEntity<Shows> updateUser(@PathVariable("titulo") String tituloRecibido, @RequestBody Shows show) {
+        Shows existingShow = this.repo.findByTitulo(tituloRecibido);
+        if (existingShow != null) {
+        	existingShow.setShowId(show.getShowId());
+        	existingShow.setTitulo(show.getTitulo());
+        	existingShow.setActores(show.getActores());
+        	existingShow.setAnyoLanzamiento(show.getAnyoLanzamiento());
+        	existingShow.setDescripcion(show.getDescripcion());
+        	existingShow.setDirector(show.getDirector());
+        	existingShow.setDuracion(show.getDuracion());
+        	existingShow.setPais(show.getPais());
+        	existingShow.setTags(show.getTags());
+        	existingShow.setRating(show.getRating());
+        	existingShow.setFecha(show.getFecha());
+        	existingShow.setTipo(show.getTipo());
+        	this.repo.save(existingShow);
+            return new ResponseEntity<>(existingShow, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 	
 }
--- src/main/java/com/example/repositorio/ShowsRepository.java
+++ src/main/java/com/example/repositorio/ShowsRepository.java
@@ -6,8 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.modelo.Shows;
 
+import jakarta.transaction.Transactional;
+
 public interface ShowsRepository extends JpaRepository<Shows,String> {
 	
 	List<Shows> findBytipoStartingWith(String tipo);
-
+	
+	@Transactional
+    long deleteByTitulo(String titulo);
+	
+	@Transactional
+	Shows findByTitulo(String titulo);
 }
